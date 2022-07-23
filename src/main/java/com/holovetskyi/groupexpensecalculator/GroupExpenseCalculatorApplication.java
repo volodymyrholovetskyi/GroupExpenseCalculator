8f1127c6c4cef29c2d5891d5071437c40b73ea5c
Volodymyr <volodymyrgolovetskyi1995@gmail.com>
2022-07-23 11:41:25 +0200

    start project and add file README.md

---
+++ src/main/java/com/holovetskyi/groupexpensecalculator/GroupExpenseCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.holovetskyi.groupexpensecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupExpenseCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupExpenseCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/holovetskyi/groupexpensecalculator/GroupExpenseCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holovetskyi.groupexpensecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupExpenseCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
